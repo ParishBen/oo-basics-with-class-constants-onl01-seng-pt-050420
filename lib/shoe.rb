522fef3d9e8300596eab1e53a12d8cdb4f9199be
Ben Parish <parishben13@gmail.com>
2020-05-21 04:42:01 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,7 +5,7 @@ BRANDS = []
   def initialize(brand)
     @brand = brand
     BRANDS<<@brand
-    BRANDS.each.with_index do |brand, ind| if @brand == brand[ind]
+    BRANDS.each.with_index do |brand, ind| if @brand = brand[ind]
     BRANDS.pop
    end
   end
